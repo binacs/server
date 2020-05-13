c9c45f54f4b0c235e81be6788a3bdfcbf45901d7
BinacsLee <bin646891055@gmail.com>
2020-05-13 15:13:48 +0800

    [script] support systemd

---
+++ start-systemd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo cp ./bin/server /usr/sbin/binacsserver
+
+cat <<EOF | sudo tee /etc/systemd/system/binacsserver.service
+[Unit]
+Description=BinacsServer
+Documentation=https://github.com/BinacsLee/server
+[Service]
+ExecStart=/usr/sbin/binacsserver start \\
+  --configFile=${configfile} 
+Restart=on-failure
+RestartSec=5
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable binacsserver
+sudo systemctl start binacsserver
